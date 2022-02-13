4f094894dc9c5cc0cb2e27a4cd27228a632fa19f
Sashikbear <sashikgr@gmail.com>
2022-02-13 22:05:43 +0200

    Add functionality to constructor inputs

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -25,6 +25,16 @@ const cardGrid = document.querySelector(".card-popup__backgrounds-grid");
 const backgroundPopup = document.querySelector(".card-popup");
 const closeButton = document.querySelector(".card-item__button_type_close");
 const cardPrototype = document.querySelector(".card__form_type_text-fields");
+const inputAll = document.querySelectorAll(".card__text-input");
+const inputColourTo = document.querySelector(".card__text-input_type_to");
+const inputColourFrom = document.querySelector(".card__text-input_type_from");
+const iputColourMessage = document.querySelector(
+  ".card__text-input_type_textarea"
+);
+const colourToPicker = document.querySelector("#to-picker");
+const colourFromPicker = document.querySelector("#from-picker");
+const colourMessagePicker = document.querySelector("#text-picker");
+const fontSelector = document.querySelector("#fonts");
 function openPopup(popup) {
   popup.classList.add("card-popup_opened");
   document.addEventListener("keydown", closeByEscape);
@@ -49,13 +59,31 @@ function setBackground() {}
 backgroundOptions.forEach((option) => {
   const backgroundItem = document.createElement("li");
   backgroundItem.classList.add("card-popup__background-item");
-  backgroundItem.style.backgroundImage = "url(" + option.url + ")";
+  backgroundItem.style.backgroundImage = `url("${option.url}")`;
   backgroundItem.addEventListener("click", (evt) => {
     backgroundButton.style.backgroundImage = evt.target.style.backgroundImage;
     cardPrototype.style.backgroundImage = evt.target.style.backgroundImage;
   });
   cardGrid.append(backgroundItem);
 });
+function changeColour(evt, input) {
+  input.style.color = evt.target.value;
+}
+function changeFont(evt, inputs) {
+  inputs.forEach((input) => {
+    input.style.fontFamily = `${evt.target.value}`;
+  });
+}
 
 backgroundButton.addEventListener("click", () => openPopup(backgroundPopup));
 closeButton.addEventListener("click", () => closePopup(backgroundPopup));
+colourToPicker.addEventListener("change", (evt) =>
+  changeColour(evt, inputColourTo)
+);
+colourFromPicker.addEventListener("change", (evt) =>
+  changeColour(evt, inputColourFrom)
+);
+colourMessagePicker.addEventListener("change", (evt) =>
+  changeColour(evt, iputColourMessage)
+);
+fontSelector.addEventListener("change", (evt) => changeFont(evt, inputAll));
