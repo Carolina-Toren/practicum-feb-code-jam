8644f7af811498177ed96987c6f57eb8f7c53c73
Carolina Toren <54749413+Carolina-Toren@users.noreply.github.com>
2022-02-15 16:24:53 +0200

    playlist functions and scrollbar

---
+++ src/pages/playlist/scripts/playlist.js
@@ -0,0 +1,98 @@
+// import music from "./utls/constants";
+
+const music = [
+  {
+    name: "ron gelinas: chillout lounge where will i go",
+    url: "https://vocaroo.com/embed/1nvNMYF35O4d?autoplay=0",
+  },
+
+  {
+    name: "Warm Memories: Emotional Inspiring Piano",
+    url: "https://vocaroo.com/embed/1o0jXXVF0EF1?autoplay=0",
+  },
+  {
+    name: "Luke Bergs: Soulful",
+    url: "https://vocaroo.com/embed/1owA9rYjR82W?autoplay=0",
+  },
+  {
+    name: "Blooming Melody",
+    url: "https://vocaroo.com/embed/15L0IDGHLWuK?autoplay=0",
+  },
+  {
+    name: "Jana-Katharina: Somebody That I Used To Know Cover x Spheria Instrumental",
+    url: "https://vocaroo.com/embed/1f1xHPgVuPoO?autoplay=0",
+  },
+  {
+    name: "Kevin MacLeod: Parisian",
+    url: "https://vocaroo.com/embed/141Twmi30Fsz?autoplay=0",
+  },
+  {
+    name: "10 Slainte She Moved Through the Fair",
+    url: "https://vocaroo.com/embed/17gV7R4M0pQW?autoplay=0",
+  },
+  {
+    name: "On Hold for You",
+    url: "https://vocaroo.com/embed/1cTCtGGabUoL?autoplay=0",
+  },
+  {
+    name: "Bad Love Vocal",
+    url: "https://vocaroo.com/embed/141G84QpmHwz?autoplay=0",
+  },
+  {
+    name: "Love Me Harder",
+    url: "https://vocaroo.com/embed/15qWzzlsXUif?autoplay=0",
+  },
+  {
+    name: "M6: Nervous",
+    url: "https://vocaroo.com/embed/1atA5b2oanI3?autoplay=0",
+  },
+  {
+    name: "Smart",
+    url: "https://vocaroo.com/embed/18IFdORx3uWg?autoplay=0",
+  },
+  {
+    name: "Love life",
+    url: "https://vocaroo.com/embed/17c8CDuZ6jRc?autoplay=0",
+  },
+  {
+    name: "Al andalus",
+    url: "https://vocaroo.com/embed/11moLnljaemV?autoplay=0",
+  },
+  {
+    name: "Monplaisir: Everything is true",
+    url: "https://vocaroo.com/embed/14r6tU0m4srN?autoplay=0",
+  },
+  {
+    name: "Komiku: Sexy Time",
+    url: "https://vocaroo.com/embed/15m9xqydftBZ?autoplay=0",
+  },
+];
+
+// export default music;
+
+const playlistItemTemplate = document.querySelector("#playlist-block").content.querySelector(".playlists__item");
+const playlistContainerYour = document.querySelector("#your-playlist");
+const playlistContainerOur = document.querySelector("#our-playlist");
+
+function playlistGenerator({ name, url }) {
+  const playlistItem = playlistItemTemplate.cloneNode(true);
+  const playlistName = playlistItem.querySelector(".playlists__item-name");
+  const playlistUrl = playlistItem.querySelector(".playlist__iframe");
+  const playlistCheckbox = playlistItem.querySelector(".playlists__item-checkbox");
+  playlistName.textContent = name;
+  playlistUrl.src = url;
+  playlistUrl.alt = name;
+  playlistCheckbox.addEventListener("click", (e) => {
+    moveToYourPlatlist(playlistItem);
+  });
+  return playlistItem;
+}
+
+music.forEach((musicData) => {
+  playlistContainerOur.append(playlistGenerator(musicData));
+});
+
+function moveToYourPlatlist(itemData) {
+  this._item = itemData;
+  console.log("this is ", this._item);
+}
---
+++ src/pages/playlist/scripts/utls/constants.js
@@ -0,0 +1,69 @@
+const music = [
+  {
+    name: "ron gelinas: chillout lounge where will i go",
+    url: "https://vocaroo.com/embed/1nvNMYF35O4d?autoplay=0",
+  },
+
+  {
+    name: "Warm Memories: Emotional Inspiring Piano",
+    url: "https://vocaroo.com/embed/1o0jXXVF0EF1?autoplay=0",
+  },
+  {
+    name: "Luke Bergs: Soulful",
+    url: "https://vocaroo.com/embed/1owA9rYjR82W?autoplay=0",
+  },
+  {
+    name: "Blooming Melody",
+    url: "https://vocaroo.com/embed/15L0IDGHLWuK?autoplay=0",
+  },
+  {
+    name: "Jana-Katharina: Somebody That I Used To Know Cover x Spheria Instrumental",
+    url: "https://vocaroo.com/embed/1f1xHPgVuPoO?autoplay=0",
+  },
+  {
+    name: "Kevin MacLeod: Parisian",
+    url: "https://vocaroo.com/embed/141Twmi30Fsz?autoplay=0",
+  },
+  {
+    name: "10 Slainte She Moved Through the Fair",
+    url: "https://vocaroo.com/embed/17gV7R4M0pQW?autoplay=0",
+  },
+  {
+    name: "On Hold for You",
+    url: "https://vocaroo.com/embed/1cTCtGGabUoL?autoplay=0",
+  },
+  {
+    name: "Bad Love Vocal",
+    url: "https://vocaroo.com/embed/141G84QpmHwz?autoplay=0",
+  },
+  {
+    name: "Love Me Harder",
+    url: "https://vocaroo.com/embed/15qWzzlsXUif?autoplay=0",
+  },
+  {
+    name: "M6: Nervous",
+    url: "https://vocaroo.com/embed/1atA5b2oanI3?autoplay=0",
+  },
+  {
+    name: "Smart",
+    url: "https://vocaroo.com/embed/18IFdORx3uWg?autoplay=0",
+  },
+  {
+    name: "Love life",
+    url: "https://vocaroo.com/embed/17c8CDuZ6jRc?autoplay=0",
+  },
+  {
+    name: "Al andalus",
+    url: "https://vocaroo.com/embed/11moLnljaemV?autoplay=0",
+  },
+  {
+    name: "Monplaisir: Everything is true",
+    url: "https://vocaroo.com/embed/14r6tU0m4srN?autoplay=0",
+  },
+  {
+    name: "Komiku: Sexy Time",
+    url: "https://vocaroo.com/embed/15m9xqydftBZ?autoplay=0",
+  },
+];
+
+export default music;
