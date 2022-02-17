d385aa28d0736d156e1c70bfff25bebb73320169
Sashikbear <sashikgr@gmail.com>
2022-02-17 15:45:33 +0200

    Add more items to the idea arrays

--- src/pages/index/scripts/utils/constants.js
+++ src/pages/index/scripts/utils/constants.js
@@ -3,19 +3,26 @@ export const selfcareIdeas = [
   "Create some art for your walls. Use what you have on hand, tack up some old photos, and decorate your space with what inspires you.",
   "Bake yourself some fresh bread. Embrace the slowness of the process, indulge in the physical presence that kneading requires, take in the smell of the dough as it rises. Making bread costs very little, but the reward is always luxurious.",
   "Lay out a blanket and stargaze or cloudgaze. There is no goal, other than stretching your eyes and succumbing to your imagination.",
+  "Explore local nature trails.We guarantee that you'll discover something new, and you'll capture pretty, inspiring nature content along the way.",
+  "Head to the beach. Or lake. Or river. Or pond. Just any body of water, really. It'll give you a moment of Zen.",
   "Plant something—gardening has immense mental and emotional benefits. If you don't have land, nurture a houseplant. If you don't have houseplants, go outside and gently touch the plants on a morning walk. Stop and smell the roses, and enjoy the odd stares from passersby.",
 ];
 export const friendsIdeas = [
-  "Go On A Scavenger Hunt. An old fashioned scavenger hunt can be a great way to have some friendly competition while enjoying your friendships!",
+  "Go on a scavenger hunt. An old fashioned scavenger hunt can be a great way to have some friendly competition while enjoying your friendships!",
   "Volunteer. Like the old adage says, it’s better to give than to receive. There are plenty of causes, from animal shelters to soup kitchens, which would appreciate our earnest help.",
   "Go to a Paint (& Wine) Night. You might have heard about these creative events where you can learn to paint a masterpiece while sipping on wine, but now it’s time to try it out for yourself! ",
+  "Play some board games.There's nothing better than a classic game night. Have everyone bring their game (and drink) of choice for a night of competitive fun.",
+  "Go on a biking adventure. Hop on a bike share with nowhere to go and see where the day leads. The best journeys are usually unplanned, right?",
+  "Host a potluck dinner. Potlucks bring everyone together for the best reason of all: to eat! And a lot.",
   "Bake Cupcakes. Let’s face it. It’s enjoyable to eat just the frosting. But try to refrain, and set up shop on the kitchen counter for some serious baking. Find a yummy recipe online or get inspired by the signature cocktail and try creating your own recipe. ",
 ];
 export const othersIdeas = [
   "Spend some time putting together playlists for each other on your favorite streaming service. Choose songs that spark memories of your relationship or simply tunes you know your significant other will love. Then pour some wine, turn on the music, and reminisce.",
   "Even if it's cold where you live, taking a scenic hike is a great way to unplug and unwind this Valentine's Day. ",
   "You don't need a telescope to set up a stargazing date. Just head to your own backyard with a warm, outdoor blanket and use a stargazing app to help identify constellations in the night sky.",
+  "Have a picnic in the park. All you need is a blanket, some snacks, wine obv, and a portable speaker for a relaxing quality time with the people that matter.",
   "Is there a landmark in your town that you've always wanted to visit? Or a museum you've never seen? Become tourists in your own home and finally check off one or two of those bucket list items you keep putting off. Get into the role by snapping tons of pictures throughout the day.",
+  "Tap into your inner child at the playground. When was the last time you spent an afternoon on the monkey bars and swing sets?",
   "Take the day off from work and enjoy some R&R. Have breakfast in bed, do a crossword together, and binge-watch your favorite shows.",
 ];
 export const selfcare = document.querySelector(
